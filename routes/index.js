6fc45f80deff31c23ef6d96f30428696ddad84a0
gauntc <gaunt.caleb@gmail.com>
2017-12-13 12:40:06 -0700

    Added backend routes

--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,90 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/activityDB', {useMongoClient:true});
 require('../models/Activities');
 var Activity = mongoose.model('Activity');
+var users = require('../controllers/users_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    console.log("/ Route");
+//    console.log(req);
+    console.log(req.session);
+    if (req.session.user) {
+      console.log("/ Route if user");
+        res.render('index', { title: 'Express' });
+    }
+    else {
+      console.log("/ Route else user");
+      req.session.msg = 'Access denied!';
+      res.redirect('/login');
+    }
 });
 
 router.post('/activities', function(req, res, next) {
-  var activity = new Activity(req.body);
-  activity.save(function(err, activity) {
-    if(err){ return next(err); }
-    res.json(activity);
-  });
+  if (req.session.user) {
+    var activity = new Activity(req.body);
+    activity.save(function(err, activity) {
+      if(err){ return next(err); }
+      res.json(activity);
+    });
+  }
+  else {
+    res.redirect('/login');
+  }
 });
 
-router.get('/activities/:activity', function(req, res) {
-  res.json(req.activity);
-});
-
+// router.get('/activities/:activity', function(req, res) {
+//   res.json(req.activity);
+// });
 
 router.get('/activities', function(req,res) {
-  Activity.find(function(err, activities) {
-    if(err){ return next(err); }
-    res.json(activities);
-  })
+    if (req.session.user) {
+      Activity.find({username : req.body.username}, function(err, activities) {
+        if(err) { return next(err); }
+        res.json(activities);
+      })
+    }
+    else {
+      res.redirect('/login');
+    }
 })
 
-router.delete('/activities', function(req, res, next) {
-  console.log("in delete");
-  Activity.find().remove(function(err) {
-    console.log("it ran");
-    if(err){return console.error(err)};
-    res.sendStatus(200);
+// router.delete('/:userId/activities', function(req, res, next) {
+//   console.log("in delete");
+//   Activity.find().remove(function(err) {
+//     console.log("it ran");
+//     if(err){return console.error(err)};
+//     res.sendStatus(200);
+//   });
+//
+// });
+
+router.get('/signup', function(req, res) {
+    console.log("/signup Route");
+    if(req.session.user) {
+      res.redirect('/');
+    }
+    res.render('signup', {msg:req.session.msg});
+});
+
+router.get('/login', function(req, res) {
+    console.log("/login Route");
+    if(req.session.user) {
+      res.redirect('/');
+    }
+    res.render('login', {msg:req.session.msg});
+});
+
+router.get('/logout', function(req, res) {
+    console.log("/logout Route");
+    req.session.destroy(function() {
+      res.redirect('/login');
+    });
   });
 
-});
+router.post('/signup', users.signup);
+router.post('/user/update', users.updateUser);
+router.post('/user/delete', users.deleteUser);
+router.post('/login', users.login);
+router.get('/user/profile', users.getUserProfile);
 
 module.exports = router;
