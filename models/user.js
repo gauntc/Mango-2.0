c27b634b920a83720a79c4bcff115826dc9d6c49
gauntc <gaunt.caleb@gmail.com>
2017-12-13 17:50:19 -0700

    Fix data model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var UserSchema= new mongoose.Schema({
 	username: {type: String, unique: true },
-	email: String;
-	hashed_password: String;
+	email: String,
+	hashed_password: String
 });
 
 mongoose.model('User', UserSchema);
