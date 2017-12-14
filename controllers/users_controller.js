987f90bcd2a9cfca0ae9ceb23d956b322d859e46
gauntc <gaunt.caleb@gmail.com>
2017-12-13 17:47:20 -0700

    More fixing backend issues

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
-var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 function hashPW(pwd) {
   return crypto.createHash('sha256').update(pwd).
--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 var UserSchema= new mongoose.Schema({
 	username: {type: String, unique: true },
 	email: String;
 	hashed_password: String;
 });
-	
+
+mongoose.model('User', UserSchema);
